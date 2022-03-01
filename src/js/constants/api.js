61b2b9ff34903e46aba705d19bcf097cc9345409
Nguyen Thao Tra <nguyen.trathao@asnet.com.vn>
2022-03-01 14:54:52 +0700

    fix: update code styles

--- src/js/constants/api.js
+++ src/js/constants/api.js
@@ -1,4 +1,4 @@
 export const API = {
   BASE_URL: 'http://localhost:3000',
-  USER_URL: '/users'
-}
+  USER_URL: '/users',
+};
--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -1,19 +1,19 @@
-import STORAGE_KEYS from "../constants/storage-keys";
+import STORAGE_KEYS from '../constants/storage-keys';
 
 /**
  * @class Storage
  * Manage data stored in the browser
  */
 export default class Storage {
- static setItem(data) {
+  static setItem(data) {
     localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(data));
   }
 
- static getItem() {
+  static getItem() {
     JSON.parse(localStorage.getItem(STORAGE_KEYS.USER));
   }
 
- static removeItem() {
+  static removeItem() {
     localStorage.removeItem(STORAGE_KEYS.USER);
   }
 }
