b4b71cdc5f9b3bc7cb8b705673afba76dfa8b917
Nguyen Thao Tra <nguyen.trathao@asnet.com.vn>
2022-03-02 08:07:00 +0700

    fix: user common in view post list

--- src/js/views/post-list.js
+++ src/js/views/post-list.js
@@ -1,66 +1,61 @@
 import Storage from '../utils/storage';
-import { checkLogin, getElementById } from '../utils/common';
+import { checkLogin, getElementById, setTextContent } from '../utils/common';
 
 /**
  * @class PostListView
  * Visual representation of the home page.
  */
- export default class PostListView {
-   constructor() {
-     this.ulElement = getElementById('post-list');
-     this.postTemplate = getElementById('post-template');
-     this.iconsTemplate = getElementById('icons-template');
-     this.user = Storage.getItem();
-     checkLogin();
-   }
+export default class PostListView {
+  constructor() {
+    this.ulElement = getElementById('post-list');
+    this.postTemplate = getElementById('post-template');
+    this.iconsTemplate = getElementById('icons-template');
+    this.user = Storage.getItem();
+    checkLogin();
+  }
 
-   /**
-    * Create post element
-    * When user login show icon edit, remove post created by user
-    * @param {array} posts
-    */
-   createPostElement(post) {
-     if (!post) return;
+  /**
+   * Create post element
+   * When user login show icon edit, remove post created by user
+   * @param {array} posts
+   */
+  createPostElement(post) {
+    if (!post) return;
 
-     const { title, user, createdDate, type } = post;
+    const { title, user, createdDate, type } = post;
 
-     const liElement = this.postTemplate.content.firstElementChild.cloneNode(true);
-     if (!liElement) return;
+    const liElement = this.postTemplate.content.firstElementChild.cloneNode(true);
+    if (!liElement) return;
 
-     const titleElement = liElement.querySelector('[data-id="title"]');
-     if (titleElement) titleElement.textContent = title;
+    setTextContent(liElement, '[data-id="title"]', title);
+    setTextContent(liElement, '[data-id="name"]', user.userName);
+    setTextContent(liElement, '[data-id="date"]', createdDate);
+    setTextContent(liElement, '[data-id="type"]', type);
 
-     const userElement = liElement.querySelector('[data-id="name"]');
-     if (userElement) userElement.textContent = user.userName;
+    if (this.user && this.user.id === post.userId) {
+      const iconElement = this.iconsTemplate.content.cloneNode(true);
+      if (!iconElement) return;
 
-     const dateElement = liElement.querySelector('[data-id="date"]');
-     if (dateElement) dateElement.textContent = createdDate;
+      this.menuIcon = liElement.querySelector('[data-id="menu"]');
+      if (this.menuIcon){
+        this.menuIcon.appendChild(iconElement);
+      }
+    }
 
-     const typeElement = liElement.querySelector('[data-id="type"]');
-     if (typeElement) typeElement.textContent = type;
+    return liElement;
+  }
 
-     if (this.user && this.user.id === post.userId) {
-       const iconElement = this.iconsTemplate.content.cloneNode(true);
-       if (!iconElement) return;
+  /**
+   * Render post list
+   * Use a ul and li to display the posts
+   * @param {array} posts
+   */
+  renderPostList(posts) {
+    if (!Array.isArray(posts) || posts.length === 0) return;
 
-       this.menuIcon = liElement.querySelector('[data-id="menu"]');
-       if (this.menuIcon) this.menuIcon.appendChild(iconElement);
-     }
-
-     return liElement;
-   }
-
-   /**
-    * Render post list
-    * Use a ul and li to display the posts
-    * @param {array} posts
-    */
-   renderPostList(posts) {
-     if (!Array.isArray(posts) || posts.length === 0) return;
-
-     posts.forEach((post) => {
-       const liElement = this.createPostElement(post);
-       this.ulElement.appendChild(liElement);
-     });
-   }
- }
+    posts.forEach((post) => {
+      const liElement = this.createPostElement(post);
+      this.ulElement.appendChild(liElement);
+    });
+  }
+}
